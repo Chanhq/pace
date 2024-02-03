aa5a3dbcce3111a6f92f4ccc93ab53761f3fa2ca
Jonas Tolkemitt <jonas.tolkemitt@gmail.com>
2024-02-03 13:22:13 +0100

    Add more benchmark methods

--- src/application.rs
+++ src/application.rs
@@ -1,12 +1,41 @@
-use std::{fs, path, time::Instant};
+use std::{
+    fs::{self, File},
+    io::{LineWriter, Write},
+    path,
+    time::Instant,
+};
 
-use crate::{error::Error, graph::{self, Graph}, graph_builder::GraphBuilder};
+use crate::{
+    error::Error,
+    graph::{self, Graph},
+    graph_builder::GraphBuilder,
+};
 
 pub struct Application {}
 
 impl Application {
     pub fn new() -> Application {
         Application {}
+    }
+
+    pub fn run_test_on_randomly_generated_graphs(&self) -> Result<(), Error> {
+        let file = File::create("benchmark_results.txt")?;
+        let mut file = LineWriter::new(file);
+
+        self.run_test_on_randomly_generated_graph(5_000, 5_000, 2_500_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(10_000, 10_000, 10_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(50_000, 50_000, 10_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(100_000, 10_000, 10_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(500_000, 500_000, 10_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(1_000_000, 1_000_000, 10_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(1_000_000, 1_000_000, 20_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(1_000_000, 1_000_000, 30_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(1_000_000, 1_000_000, 50_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(1_000_000, 1_000_000, 100_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(1_000_000, 1_000_000, 250_000_000, &mut file)?;
+        self.run_test_on_randomly_generated_graph(1_000_000, 1_000_000, 500_000_000, &mut file)?;
+
+        Ok(())
     }
 
     pub fn run(&self) -> Result<(), Error> {
@@ -34,46 +63,105 @@ impl Application {
 
         Ok(())
     }
-
-    pub fn test(&self) -> Result<(), Error> {
-        let graph = GraphBuilder::build_random_graph(3, 3, 9)?;
-        let ordering = graph.sort_fas();
-
-        let res = graph.compute_number_of_crossings_for_ordering(&ordering);
-
-        println!("Crossings: {res:?}");
-
-        Ok(())
-    }
 }
 
 impl Application {
-    fn run_test_on_graph_from_file(&self, filename: &str, should_print_ordering: bool, should_compute_number_of_crossings: bool) -> Result<(), Error> {
+    fn run_test_on_graph_from_file(
+        &self,
+        filename: &str,
+        should_print_ordering: bool,
+        should_compute_number_of_crossings: bool,
+    ) -> Result<(), Error> {
         println!("Loading Graph from file '{filename}'.");
         let begin = Instant::now();
         let graph = GraphBuilder::build_graph_from_file(filename)?;
-        println!("Finished loading graph. ({} ms)", begin.elapsed().as_millis());
-        println!("The graph has {} fixed nodes, {} free nodes and {} edges.", graph.number_of_fixed_nodes(), graph.number_of_free_nodes(), graph.number_of_edges());
-        self.run_test_on_graph(&graph, should_print_ordering, should_compute_number_of_crossings)?;
+        println!(
+            "Finished loading graph. ({} ms)",
+            begin.elapsed().as_millis()
+        );
+        println!(
+            "The graph has {} fixed nodes, {} free nodes and {} edges.",
+            graph.number_of_fixed_nodes(),
+            graph.number_of_free_nodes(),
+            graph.number_of_edges()
+        );
+        self.run_test_on_graph(
+            &graph,
+            should_print_ordering,
+            should_compute_number_of_crossings,
+        )?;
 
         Ok(())
     }
 
-    fn run_test_on_graph(&self, graph: &Graph, should_print_ordering: bool, should_compute_number_of_crossings: bool) -> Result<(), Error> {
+    fn run_test_on_randomly_generated_graph(
+        &self,
+        number_of_fixed_nodes: usize,
+        number_of_free_nodes: usize,
+        number_of_edges: usize,
+        file: &mut LineWriter<File>,
+    ) -> Result<(), Error> {
+        let begin = Instant::now();
+        println!(
+            "Generating graph with {} fixed nodes, {} free nodes and {} edges.",
+            number_of_fixed_nodes, number_of_free_nodes, number_of_edges
+        );
+        let graph = GraphBuilder::build_random_graph(
+            number_of_fixed_nodes,
+            number_of_free_nodes,
+            number_of_edges,
+        )?;
+        let generation_elapsed = begin.elapsed().as_millis();
+        println!("Finished generating graph. ({} ms)", generation_elapsed);
+
+        let begin = Instant::now();
+        println!("Computing ordering.");
+        graph.sort_fas();
+        let ordering_elapsed = begin.elapsed().as_millis();
+        println!("Finished computing ordering. ({} ms)", ordering_elapsed);
+        println!("");
+
+        write!(
+            file,
+            "#fixed: {}\n#free: {}\n#edges: {}\ngeneration: {} ms\nordering: {} ms\n\n",
+            graph.number_of_fixed_nodes(),
+            graph.number_of_free_nodes(),
+            graph.number_of_edges(),
+            generation_elapsed,
+            ordering_elapsed
+        )?;
+
+        Ok(())
+    }
+
+    fn run_test_on_graph(
+        &self,
+        graph: &Graph,
+        should_print_ordering: bool,
+        should_compute_number_of_crossings: bool,
+    ) -> Result<(), Error> {
         if should_compute_number_of_crossings {
             let begin = Instant::now();
             println!("Computing number of crossings for default ordering.");
             let number_of_crossings = graph.compute_number_of_crossings_with_default_ordering()?;
-            println!("The graph has {} crossings. ({} ms)",number_of_crossings , begin.elapsed().as_millis());
+            println!(
+                "The graph has {} crossings. ({} ms)",
+                number_of_crossings,
+                begin.elapsed().as_millis()
+            );
         }
 
         let begin = Instant::now();
         println!("Computing ordering for free nodes.");
         let ordering = graph.sort_fas();
-        println!("Finished computing ordering. ({} ms)", begin.elapsed().as_millis());
+        println!(
+            "Finished computing ordering. ({} ms)",
+            begin.elapsed().as_millis()
+        );
 
         if should_print_ordering {
-            let ordering_with_actual_node_name: Vec<usize> = ordering.iter().map(|x| x + 1).collect();
+            let ordering_with_actual_node_name: Vec<usize> =
+                ordering.iter().map(|x| x + 1).collect();
             println!("Ordering: {:?}", ordering_with_actual_node_name);
         }
 
@@ -81,7 +169,11 @@ impl Application {
             let begin = Instant::now();
             println!("Computing number of crossings for computed ordering.");
             let number_of_crossings = graph.compute_number_of_crossings_for_ordering(&ordering)?;
-            println!("The graph has {} crossings with the new ordering. ({} ms)",number_of_crossings , begin.elapsed().as_millis());
+            println!(
+                "The graph has {} crossings with the new ordering. ({} ms)",
+                number_of_crossings,
+                begin.elapsed().as_millis()
+            );
         }
 
         println!("");
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use pace::application::Application;
 fn main(){
     let application = Application::new();
 
-    application.run_tests_on_tiny_test_set().unwrap();
+    application.run_test_on_randomly_generated_graphs().unwrap();
 }
