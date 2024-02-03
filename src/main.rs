6953331580b8cde5e8cd9b575011cf0849675a48
Jonas Tolkemitt <jonas.tolkemitt@gmail.com>
2024-02-03 17:39:07 +0100

    Change the way the benchmark files are generated

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use pace::application::Application;
 
+
 fn main(){
     let application = Application::new();
 
-    application.run_benchmark_for_20_million_edges().unwrap();
     application.run_benchmark_for_1_million_fixed_and_free_nodes().unwrap();
+    application.run_benchmark_for_20_million_edges().unwrap();
 }
