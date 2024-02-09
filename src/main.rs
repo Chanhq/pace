f2c4549d6356d765452c3b62528a0d25d206488e
Jonas Tolkemitt <jonas.tolkemitt@gmail.com>
2024-02-09 19:55:40 +0100

    Run all tests when running the program

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ fn main(){
     let application = Application::new();
 
     application.run_tests_with_same_edges_and_free_nodes().unwrap();
+    application.run_on_specific_case(500, 20).unwrap();
+    application.run_tests_on_tiny_test_set().unwrap();
+    application.run_tests_with_same_edges().unwrap();
+    application.run_tests_with_same_nodes().unwrap();
 }
